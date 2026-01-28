8e1a526b074d9db3879dd6832ae4cb0126933841
realkendo <kennethistifanus1@gmail.com>
2026-01-28 15:14:28 +0100

    migration file updates

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 import express from "express";
+import { config } from "dotenv";
+import { connectDB, disconnectDb } from "./config/db.js";
 
 // importing routes
 import moviesRoutes from "./routes/movieRoutes.js";
+
+// db coonection
+config();
+connectDB();
 
 const app = express(); //app instaciation
 const PORT = 5001; // setting port
