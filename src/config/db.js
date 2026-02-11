efb9c8e3400c6ea6bba006faf38419b66dc2b231
realkendo <kennethistifanus1@gmail.com>
2026-02-12 00:34:46 +0100

    prisma and neon serverless config

--- src/config/db.js
+++ src/config/db.js
@@ -1,24 +1,35 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import dotenv from "dotenv";
+dotenv.config();
 
-const adapter = new PrismaPg({
-  url: process.env["DATABASE_URL"],
-});
+import { PrismaClient } from "@prisma/client";
+
+// console.log("ENV CHECK:", process.env.DATABASE_URL);
+
+// creating a neon databse instance
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
+// const sql = neon(connectionString);
+// const adapter = new PrismaNeon(sql);
 
 const prisma = new PrismaClient({
-  adapter,
   log:
-    process.env.NODE_ENV === "development"
-      ? ["query", "error", "warn"]
-      : ["error"],
+    connectionString === "development" ? ["query", "error", "warn"] : ["error"],
 });
 
 const connectDB = async () => {
   try {
     await prisma.$connect();
-    console.log("Database connected via Prisma");
+    console.log("✅ Database connected to Neon");
   } catch (error) {
-    console.log(`Database connection error: ${error.message}`);
+    console.log(`❌ Database connection error: ${error.message}`);
+    console.log(
+      "Connection string used:",
+      process.env.DATABASE_URL?.split("@")[1]?.split("/")[0],
+    );
     process.exit(1);
   }
 };
